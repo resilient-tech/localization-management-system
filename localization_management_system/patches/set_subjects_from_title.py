82d869ddf68b8bc1af081ff7e51501cd163acedd
Abdeali Chharchhoda <abdealiking786@gmail.com>
2026-01-20 18:45:06 +0530

    chore: minor fix

--- localization_management_system/patches/set_subjects_from_title.py
+++ localization_management_system/patches/set_subjects_from_title.py
@@ -3,4 +3,4 @@ import frappe
 
 def execute():
     EL = frappe.qb.DocType("ERPNext Localization")
-    frappe.qb.update(EL).set(EL.subject, EL.title).run()
+    frappe.qb.update(EL).set(EL.subject, EL.title).where((EL.subject.isnull()) | (EL.subject == "")).run()
